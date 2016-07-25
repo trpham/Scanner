f79b73ad02924f5de2fc905340f851ff6dd83a17
Truong Pham <phamcse@berkeley.edu>
2016-07-24 21:38:33 -0700

    What Number App

--- Scanner/ViewController.swift
+++ Scanner/ViewController.swift
@@ -11,26 +11,35 @@ import UIKit
 class ViewController: UIViewController,
                       UIImagePickerControllerDelegate,
                       UINavigationControllerDelegate {
-   
     
-    @IBOutlet var messageLabel: UILabel!
-    @IBOutlet var nameField: UITextField!
+    var correct = arc4random_uniform(5)
     
-    @IBAction func buttonPressed(sender: UIButton) {
-        let nameEntered = nameField.text!
-        messageLabel.text = "Hi there \(nameEntered)"
-        nameField.text = ""
-        nameField.resignFirstResponder()
-    }
-    
+    @IBOutlet var message: UILabel!
+
+    @IBOutlet var guessField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(correct)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    @IBAction func checkButton(sender: UIButton) {
+        let guess = guessField.text!
+        let correctGuess = String(correct)
+        guessField.text = ""
+        guessField.resignFirstResponder()
+        
+        if guess == correctGuess {
+            message.text = "Correct answer."
+            message.textColor = UIColor.greenColor()
+        } else {
+            message.text = "Wrong answer, try Again."
+            message.textColor = UIColor.redColor()
+        }
+    }
 }
 
