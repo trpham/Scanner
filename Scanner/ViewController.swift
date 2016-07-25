5817d7db95140333105b0bce78929c10808a9083
Truong Pham <phamcse@berkeley.edu>
2016-07-24 21:18:44 -0700

    Hi There \(Name)

--- Scanner/ViewController.swift
+++ Scanner/ViewController.swift
@@ -11,10 +11,18 @@ import UIKit
 class ViewController: UIViewController,
                       UIImagePickerControllerDelegate,
                       UINavigationControllerDelegate {
+   
     
-    @IBOutlet weak var PhotoLibrary: UIButton!
-    @IBOutlet weak var Camera: UIButton!
-    @IBOutlet weak var ImageDisplay: UIImageView!
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var nameField: UITextField!
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        let nameEntered = nameField.text!
+        messageLabel.text = "Hi there \(nameEntered)"
+        nameField.text = ""
+        nameField.resignFirstResponder()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,22 +32,5 @@ class ViewController: UIViewController,
         super.didReceiveMemoryWarning()
     }
     
-    @IBAction func PhotoLibraryAction(sender: UIButton) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.sourceType = .PhotoLibrary
-        presentViewController(picker, animated: true, completion: nil)
-    }
-
-    @IBAction func CameraAction(sender: UIButton) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.sourceType = .Camera
-        presentViewController(picker, animated: true, completion: nil)
-    }
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        ImageDisplay.image = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
-    }
 }
 
