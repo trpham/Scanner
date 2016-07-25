99d466b3aa3d9a2c07d570e0c14194f6f64340e6
Truong Pham <phamcse@berkeley.edu>
2016-07-24 23:09:05 -0700

    Pick a Fruit

--- Scanner/ViewController.swift
+++ Scanner/ViewController.swift
@@ -8,38 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController,
+class ViewController: UIViewController,UIPickerViewDelegate,
                       UIImagePickerControllerDelegate,
                       UINavigationControllerDelegate {
     
-    var correct = arc4random_uniform(5)
+    var fruits = ["Pick a Fruit", "Apples", "Oranges", "Lemons", "Limes", "Blueberries"]
     
-    @IBOutlet var message: UILabel!
-
-    @IBOutlet var guessField: UITextField!
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var infoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(correct)
+        imageView.image = UIImage(named: "fruits.jpg")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    @IBAction func checkButton(sender: UIButton) {
-        let guess = guessField.text!
-        let correctGuess = String(correct)
-        guessField.text = ""
-        guessField.resignFirstResponder()
-        
-        if guess == correctGuess {
-            message.text = "Correct answer."
-            message.textColor = UIColor.greenColor()
-        } else {
-            message.text = "Wrong answer, try Again."
-            message.textColor = UIColor.redColor()
-        }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruits.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruits[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let fruitSelected = fruits[row]
+        switch (fruitSelected) {
+        case "Apples":
+            imageView.image = UIImage(named: "apples.jpg")
+            infoLabel.text = "These apples are red"
+        case "Oranges":
+            imageView.image = UIImage(named: "oranges.jpg")
+            infoLabel.text = "These oranges are red"
+        case "Lemons":
+            imageView.image = UIImage(named: "lemons.jpg")
+            infoLabel.text = "These apples are red"
+        case "Limes":
+            imageView.image = UIImage(named: "limes.jpg")
+            infoLabel.text = "These apples are red"
+        case "Blueberries":
+            imageView.image = UIImage(named: "blueberries.jpg")
+            infoLabel.text = "These apples are red"
+        default:
+            imageView.image = UIImage(named: "fruits.jpg")
+            infoLabel.text = "Enjoy fruits"
+        } // end of switch statement
+    
+    } // end of didSelectRow function
+   
 }
 
